04db876ffbb53091a3c024ccd2c3faae01e81ad9
Joakim <joakim.winther@gmail.com>
2023-06-12 03:38:50 +0200

    Add Heading (H1-H6) components

---
+++ src/components/typography/Heading.tsx
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+  font-size: 2.5rem;
+  font-weight: 700;
+  line-height: 120%;
+`;
+
+const H2 = styled.h2`
+  font-size: 2rem;
+  font-weight: 700;
+  line-height: 125%;
+`;
+
+const H3 = styled.h3`
+  font-size: 1.75rem;
+  font-weight: 700;
+  line-height: 129%;
+`;
+
+const H4 = styled.h4`
+  font-size: 1.5rem;
+  font-weight: 700;
+  line-height: 133%;
+`;
+
+const H5 = styled.h5`
+  font-size: 1.25rem;
+  font-weight: 700;
+  line-height: 120%;
+`;
+
+const H6 = styled.h6`
+  font-size: 1rem;
+  font-weight: 700;
+  line-height: 150%;
+  letter-spacing: 0.01rem;
+`;
+
+const Heading = {
+  H1,
+  H2,
+  H3,
+  H4,
+  H5,
+  H6,
+};
+
+export default Heading;
