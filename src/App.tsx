a2bdd3a584fa17eded1d99bd9d4cbf5309881089
Joakim <joakim.winther@gmail.com>
2023-06-12 03:06:30 +0200

    Implement routes and set up Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import viteLogo from 'assets/vite.svg';
-import reactLogo from 'assets/react.svg';
-import { Counter } from 'features/counter';
 import { store } from 'store';
 import GlobalStyle from 'layout/GlobalStyle';
-import 'App.css';
+import Home from 'pages/Home';
 
 function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
 function WrappedApp() {
   return (
     <Provider store={store}>
-      <GlobalStyle />
-      <App />
+      <BrowserRouter>
+        <GlobalStyle />
+        <App />
+      </BrowserRouter>
     </Provider>
   );
 }
